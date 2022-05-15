87821181458ef735247fc25fa1c5f9c1886ce740
AlienCodes <nigel.belderbos@outlook.com>
2022-05-15 20:19:14 +0200

    update battle.js

--- js/battle.js
+++ js/battle.js
@@ -3,58 +3,106 @@ let attack = [
     {
         "name": "Fire",
         "attack": 15,
-        "mp-cost": 5
+        "mp_cost": 5
     },
     {
         "name": "Water",
         "attack": 20,
-        "mp-cost": 7
+        "mp_cost": 7
     },
     {
         "name": "Earth",
         "attack": 25,
-        "mp-cost": 10
+        "mp_cost": 10
     },
     {
         "name": "Wind",
         "attack": 5,
-        "mp-cost": 8
+        "mp_cost": 8
 
     }];
+
+let enemy = {
+    "name": "random",
+    "level": 1,
+    "damage": 10,
+    "max_hp": 100,
+    "hp": 100,
+}
+
+
+const enemy_hp = document.querySelector('#enemyHealth');
+localStorage.setItem("hp", "200");
+localStorage.setItem("maji", "200");
 const username = document.querySelector('.username');
 const avatar = document.querySelector('.avatar');
 const hp = document.querySelector('#hp');
 const mp = document.querySelector('#mp');
 const attacks = document.querySelectorAll('.attack');
-for(let x = 0 ; x < attacks.length; x++){
+let Turn = true;
+for (let x = 0; x < attacks.length; x++) {
     attacks[x].innerHTML = attack[x].name;
 }
-/** INSERT USER INFO **/
-username.innerHTML = user.username;
-avatar.src = "img/avatar.png";
-hp.value = user.hp;
-hp.max = user.max_hp;
-mp.value = user.mp;
-mp.max = user.max_hp;
+function changeTurn() {
+    Turn = !Turn;
+    return Turn;
+}
 
 /** ##  START SCRIPT ## **/
 
-function HP_handler(damage) {
-    user.current_HP -= damage;
-    console.log(user.current_HP);
+function damage_handler(damage, consumption) {
+    if (enemy.hp <= 0) {
+        /** ##  START new enemy spawn ## **/
+    }
+    else {
+        mp.value -= consumption;
+        enemy.hp -= damage;
+        loadEnemy()
+    }
+};
+function enemy_damage_handler(damage) {
+    if (hp.value <= 0) {
+        /** ##  START new enemy spawn ## **/
+    }
+    else {
+        console.log("hit");
+        hp.value -= damage;
+
+    }
+};
+
+
+function loadEnemy() {
+    enemy_hp.value = enemy.hp
+    enemy_hp.max = enemy.max_hp;
+
 }
+
 
 function loadUser() {
-    user.current_HP = user.max_HP;
-    user.current_MP = user.max_MP;
+
+    username.innerHTML = user.username;
+    avatar.src = "img/avatar.png";
+    hp.value = JSON.parse(user.hp);
+    hp.max = JSON.parse(user.hp);
+    mp.value = JSON.parse(user.mp);
+    mp.max = JSON.parse(user.mp);
 }
 
-button = document.querySelector('.button');
-
-button.onclick = function attacks() {
-    console.log(user.current_HP);
-    HP_handler(15);
+for (let x = 0; x < attacks.length; x++) {
+    if (changeTurn() === false) {
+        attacks[x].onclick = function attacks() {
+            damage_handler(attack[x].attack, attack[x].mp_cost);
+            this.Turn = changeTurn();
+            console.log(this.Turn);
+        };
+    } else if (attacks.Turn === true) {
+        console.log("hit");
+        enemy_damage_handler(15);
+        changeTurn();
+    }
 }
 
 
+loadEnemy();
 loadUser();
