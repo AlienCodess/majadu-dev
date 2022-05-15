92b39987080339cc296ec069bf88e9f791c8b5bb
Jelle <jelledhooghe92@gmail.com>
2022-05-15 16:48:08 +0200

    edit

--- js/battle.js
+++ js/battle.js
@@ -1,18 +1,3 @@
-user = {
-    "name": "dummy",
-    "level": 1,
-    "current_HP": 0,
-    "max_HP": 100,
-    "current_MP" : 0,
-    "max_MP": 100,
-    "attacks": {
-        "attack1":{
-            "name": "fire",
-            "damage": 10,
-            "MP_consumption": 10,
-        },
-    },
-}
 
 function HP_handler(damage){
     user.current_HP -= damage;
