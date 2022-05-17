3cb64cad41e7a119a6763273db2ef5ac9face546
Jelle <jelledhooghe92@gmail.com>
2022-05-17 08:46:10 +0200

    alpine edit

--- js/template.js
+++ js/template.js
@@ -1,7 +1,8 @@
 const site_title = "Majadu";
 let sub_title;
+
 /** HEADER CREATION **/
-function createHeader(){
+function createHeader() {
     let header = document.querySelector("header");
     let header_wrapper = document.createElement("div");
     let header_title = document.createElement("div");
@@ -11,16 +12,16 @@ function createHeader(){
         "                <li><a class=\"text-decoration-none\" href=\"index.html\">Home</a></li>\n" +
         "                <li><a class=\"text-decoration-none\" href=\"info.html\">Informatie</a></li>\n" +
         "                <li><a class=\"text-decoration-none\" href=\"inloggen.html\">Inloggen</a></li>\n" +
-        "                <li><a class=\"text-decoration-none\" href=\"../registreren.html\">Registreren</a></li>\n" +
+        "                <li><a class=\"text-decoration-none\" href=\"registreren.html\">Registreren</a></li>\n" +
         "            </ul>"
 
     //header title
     p.innerHTML = site_title;
 
     //set classes
-    header_wrapper.classList.add("header_wrapper","d-flex", "justify-content-between");
-    header_title.classList.add("header_title","w-50");
-    header_nav.classList.add("header_nav","w-50");
+    header_wrapper.classList.add("header_wrapper", "d-flex", "justify-content-between");
+    header_title.classList.add("header_title", "w-50");
+    header_nav.classList.add("header_nav", "w-50");
 
     header_title.appendChild(p);
     header_wrapper.appendChild(header_title)
@@ -30,7 +31,7 @@ function createHeader(){
 }
 
 /** LEFT_NAV CREATION **/
-function createLeftnav(){
+function createLeftnav() {
     let main = document.querySelector("main");
     let left_nav = document.createElement("nav");
     let nav_ul = "<h2>Menu</h2>\n" +
@@ -38,7 +39,7 @@ function createLeftnav(){
         "            <!-- GUEST -->\n" +
         "            <li><a class=\"text-decoration-none guest\" href=\"index.html\">Home</a></li>\n" +
         "            <li><a class=\"text-decoration-none guest\" href=\"inloggen.html\">Inloggen</a></li>\n" +
-        "            <li><a class=\"text-decoration-none guest\" href=\"../registreren.html\">Registreren</a></li>\n" +
+        "            <li><a class=\"text-decoration-none guest\" href=\"registreren.html\">Registreren</a></li>\n" +
         "\n" +
         "            <!-- MEMBER -->\n" +
         "            <li><a class=\"text-decoration-none member\" href=\"#\">Instellingen</a></li>\n" +
@@ -51,7 +52,7 @@ function createLeftnav(){
 }
 
 /** FOOTER CREATION **/
-function createFooter(){
+function createFooter() {
     let footer = document.querySelector("footer");
     let p = document.createElement("p");
     p.innerHTML = "&copy; Majadu"
@@ -59,4 +60,82 @@ function createFooter(){
 }
 
 
+document.addEventListener('alpine:init', () => {
+    Alpine.data('header', () => ({
 
+        site_title: 'Majadu',
+        html: "<div class='header_wrapper d-flex justify-content-between'>" +
+            "                  <div class='header_title w-50'>" +
+            "                        <p x-text='site_title'></p>" +
+            "                    </div>\n" +
+            "                    <nav class='header_nav w-50'>" +
+            "                        <ul x-data='header'  class='d-flex justify-content-between list-style-type-none'>" +
+            "                            <template x-for='link in top_nav'>" +
+            "                                <li><a x-text='link.name' class='text-decoration-none' x-data :href='link.link'></a></li>" +
+            "                        </template>" +
+            "                    </ul>" +
+            "                </nav>" +
+            "</div>",
+        top_nav: [
+            {
+                id: 1,
+                name: 'Home',
+                link: 'index.html'
+            },
+            {
+                id: 2,
+                name: 'Info',
+                link: 'info.html'
+            }
+            ,
+            {
+                id: 3,
+                name: 'Inloggen',
+                link: 'inloggen.html'
+            }
+            ,
+            {
+                id: 4,
+                name: 'Registreren',
+                link: 'registreren.html'
+            }
+
+
+        ],
+
+    }));
+    Alpine.data('main', () => ({
+            left_nav: "<nav class='left_nav'>" +
+                "<h2>Menu</h2>" +
+                "<ul x-data='main' class='d-flex flex-direction-column list-style-type-none'>" +
+                "<template x-for='links in l_nav'>" +
+                "<li><a x-text='links.name' class='text-decoration-none' x-data :href='links.link'></a></li>" +
+                "</template>" +
+                "</ul>" +
+                "</nav>",
+            l_nav: [
+                {
+                    id: 1,
+                    name: 'Home',
+                    link: 'index.html'
+                },
+                {
+                    id: 2,
+                    name: 'Inloggen',
+                    link: 'inloggen.html'
+                }
+                ,
+                {
+                    id: 3,
+                    name: 'Registreren',
+                    link: 'registreren.html'
+                }
+            ]
+        }
+
+    ));
+    Alpine.data('footer',()=>({
+        p: '<p>&copy; Majadu</p>'
+    }))
+})
+;
