1b9ca0179d6b68d2e5144ae14100e2e05caf6589
AlienCodes <nigel.belderbos@outlook.com>
2022-05-21 18:55:05 +0200

    buttons

---
+++ js/shop.js
@@ -0,0 +1,14 @@
+const currency = document.querySelector("#currency");
+localStorage.setItem("gold","200");
+
+
+
+function buy(){
+    console.log(user.gold);
+}
+function sell(){
+    console.log("sell pressed");
+}
+function exit(){
+    console.log("exit pressed");
+}
